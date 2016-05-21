f7b0c59da9928299aeb8eec338b903acf1a49143
harlan <hello@harlankellaway.com>
2016-05-21 08:26:13 -0400

    Created new Xcode project

---
+++ refuge-ios/refuge-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 5/21/16.
+//  Copyright © 2016 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
